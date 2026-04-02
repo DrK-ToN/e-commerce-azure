0ac5db6579b1fe1e73cf0cb8230a0e6b867a8d9f
DrKToN <everton.drk@gmail.com>
2026-04-01 21:58:14 -0300

    fix: api.js

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -4,13 +4,9 @@ import axios from 'axios';
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 const api = axios.create({
-  // PRIORIDADE: 
-  // 1. Se for Localhost -> usa 3001
-  // 2. Se tiver variável de ambiente na Vercel -> usa ela
-  // 3. Fallback Final -> Link direto do seu Railway (para garantir que funcione agora)
   baseURL: isLocalhost 
     ? 'http://localhost:3001/api' 
-    : (process.env.REACT_APP_API_URL || 'https://e-commerce-azure-production.up.railway.app/api')
+    : 'https://e-commerce-azure-production.up.railway.app/api' // ADICIONE O /api AQUI
 });
 
 export default api;
